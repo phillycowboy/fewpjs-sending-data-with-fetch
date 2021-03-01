9a627302dea9ad7e81b17beccc00f3933fcbf20f
Ben Harter-Murphy <benhartermurphy@gmail.com>
2021-03-01 14:33:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+
+const baseUrl = "http://localhost:3000/users"
+
+
+
+function submitData(name, email) {
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name, email})
+    };
+
+    return fetch(baseUrl, configObj)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (object) {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message 
+            
+        });
+    
+};
+
+
+    // .catch(function(error) {
+    //     alert("BAD THINGS!");
+    //     console.log(error.message);
+    // });
